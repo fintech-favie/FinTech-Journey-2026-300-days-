361675cb6bf766e36acf28b6873d655bfec6bccf
favie <arah12anshi@gmail.com>
2026-03-09 12:44:58 +0530

    Create day2.py
    2/300 days

---
+++ day2.py
@@ -0,0 +1,25 @@
+reliance_price = 1234
+hdfc_price = 9876
+total_portfolio_value = reliance_price + hdfc_price 
+print("total value of 1 share each: ")
+print( total_portfolio_value )
+first_name = "fintech"
+last_name = "favie"
+full_name = first_name + last_name
+print("first name + last name ")
+print(full_name )
+price_a = 10
+price_b = 15
+total_price = price_a + price_b 
+print("total price ")
+print(total_price)
+x = 5
+y = 10
+z = x + y 
+a_price = 1456
+b_price = 4567
+total_price = a_price + b_price 
+print(total_price)
+part_1 = "cyber"
+part_2 = "security"
+print(part_1 + part_2 )
