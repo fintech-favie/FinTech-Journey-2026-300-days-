a40e25fea979ffca809119aa4621e810ad53c2d1
favie <arah12anshi@gmail.com>
2026-03-10 17:08:52 +0530

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,20 @@
+initial_price = 2500
+dicount_price =450
+payable_amount = initial_price - dicount_price
+print("payable amount")
+print(payable_amount)
+
+opening_balance = 10000
+withdrawl_1 = 2500
+withdrawl_2 = 1200
+remaining_balance= opening_balance - withdrawl_1 - withdrawl_2 
+print("remaining_balance")
+print(remaining_balance)
+
+gross_profit =5000
+brokerage_charges=200
+exchange_tax=50
+net_profit = gross_profit - brokerage_charges - exchange_tax 
+print("net profit")
+print(net_profit)
+
