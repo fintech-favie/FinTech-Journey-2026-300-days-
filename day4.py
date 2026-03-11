e8e48d2209c5692f780b29e43f42e451c6ddd08c
favie <arah12anshi@gmail.com>
2026-03-11 12:43:46 +0530

    Create day4.py
    Day 4: Implemented Multiplication logic for Invoice Generation and GST calculation."

---
+++ day4.py
@@ -0,0 +1,14 @@
+daily_savings = 50
+total_days = 7 
+total_pocket_money = daily_savings * total_days
+print("my total pocket money of week")
+print(total_pocket_money)
+
+item_price = 1200
+quantity = 5
+tax_rate = 0.18
+subtotal = item_price * quantity
+tax_amount = subtotal * tax_rate
+total_bill = subtotal + tax_amount
+print("-----invoice-----")
+print(total_bill)
