ef658ce5b8578a03b19de16ae22851df22927fba
favie <arah12anshi@gmail.com>
2026-03-12 14:31:40 +0530

    Create day5.py

---
+++ day5.py
@@ -0,0 +1,18 @@
+print(5/2)
+print(1000/8)
+total_bill = 125000
+total_chair = 50
+total = total_bill / total_chair
+print(total)
+amt_invest = 84000
+total_share = 30
+total = amt_invest / total_share
+print(total)
+annual = 4500000
+month = 12
+total = annual / month
+print(total)
+total_sales = 200000
+net_profit = 40000
+total = (net_profit / total_sales) * 100
+print(total)
