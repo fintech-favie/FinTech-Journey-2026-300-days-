36cbcdbb5a13fb4d291b7a8bc97cf53036885dac
favie <arah12anshi@gmail.com>
2026-03-13 22:46:53 +0530

    Create day6.py

---
+++ day6.py
@@ -0,0 +1,17 @@
+my_goal_lpa = 45
+current_skill = 6
+aim = my_goal_lpa > current_skill
+print(aim)
+
+saved_money = 5000
+exam_fee = 12000
+pay_fee = saved_money >= exam_fee 
+print("is the exam fee covered ?")
+print(pay_fee)
+
+day_challenge = 6
+day_of_challenge = 300
+days = day_challenge >= day_of_challenge
+print("am i ready for interview")
+print(days)
+
