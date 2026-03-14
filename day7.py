a6715127c7c1afba06ed03b8a661ff0f42c90548
favie <arah12anshi@gmail.com>
2026-03-14 18:42:29 +0530

    Create day7.py

---
+++ day7.py
@@ -0,0 +1,46 @@
+if 10 > 5 : 
+    print("yes , it is true ")
+else :
+	print("no , it is false")
+	
+if 1000 > 500 :
+    print("i have enough money")
+else :
+	print("i need to save more")
+	
+stock_price = 140
+buy = 150
+print("stock price ")
+if stock_price > buy :
+	print("price is low , signal - buy ")
+else :
+	print("price is high , signal - wait")
+
+stock_price = 140
+buy = 150
+print("stock price ")
+if  stock_price <= buy 
+        print("logic  :  price is low SIGNAL  :  buy now! ") # pushed  to  the  right
+ else : 
+        print("logic  :  price is high.  :  wait for now. ")  # pushed to the left
+        
+  if 10 > 5:
+    print("Yes, it is true!")
+else:
+    print("No, it is false!")
+gold_price = 72000
+my_limit = 70000
+if gold_price <= my_limit :
+	print("price is low , buy gold !!")
+else :
+	print("price is high , do not buy!!")	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
