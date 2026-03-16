13cee89d09e6bb2da4f106946c52cf7209341fef
favie <arah12anshi@gmail.com>
2026-03-16 19:56:24 +0530

    Create day9.py

---
+++ day9.py
@@ -0,0 +1,16 @@
+school_over = True 
+energy = True
+
+if school_over  ==  True and energy ==  True:
+     print("Time to study python ")
+     
+is_birthday = True 
+is_holiday = True
+
+if is_birthday == True  and  is_holiday == True:
+	print("lets celebrate")
+	
+is_tired = False
+is_not_tired = True
+if is_tired == False or is_not_tired == False:
+	print("keep coding")
