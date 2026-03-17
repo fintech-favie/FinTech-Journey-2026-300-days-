480b5b5d66b891719ba4f93a34f744fa163a2fed
favie <arah12anshi@gmail.com>
2026-03-17 19:40:19 +0530

    Create day10.py

---
+++ day10.py
@@ -0,0 +1,17 @@
+my_skill = ["code", "mmm"]
+check_skill = "mmm"
+if check_skill in my_skill:
+	print(f"success !
+	{check_skill} is part of my 45 lpa road map.")
+else:
+	print(f"time to study ! {check_skill} is missing from the plan ")
+
+watchlist = ["gold", "nifty"]
+today_target = "gold"
+print("analysis...")
+if today_target in watchlist:
+	print(f"analysis started for {today_target}....")
+	print(f"entry looks clean")
+else:
+	print(f"today_target is not in the plan...
+	stay disciplined ")
