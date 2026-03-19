f523d8fd928b22d59eb0a572448fafb3c2a0985e
favie <arah12anshi@gmail.com>
2026-03-19 13:28:58 +0530

    Create day_11.py

---
+++ day_11.py
@@ -0,0 +1,6 @@
+ The "Low Energy" Practice 🕯️
+shopping_bag = ["Lehenga", "Jhumkas", "Bangles"]
+
+# Just check if you got the most important thing!
+if "Lehenga" in shopping_bag:
+    print("✨ Navratri Outfit: READY. Time to rest now! 😴")
