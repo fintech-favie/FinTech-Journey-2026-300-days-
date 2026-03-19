ec1353b2f16cca45dd72659b33d78d80c4a1624d
favie <arah12anshi@gmail.com>
2026-03-19 21:41:58 +0530

    Create day_12.py

---
+++ day_12.py
@@ -0,0 +1,8 @@
+my_goals = ("python" , "logic")
+print(f"current goals  {my_goals} ")
+new_goal = input("what is your next big goal ?")
+my_goals.append(new_goal)
+print("goal tracker updated")
+print(f"new goals : {my_goal} ")
+print(f"total goals for the new 300 days journey")
+
