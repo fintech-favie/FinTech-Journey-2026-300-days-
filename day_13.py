1e45d127633554636821bf60a17c64a1eedf9d83
favie <arah12anshi@gmail.com>
2026-03-21 19:15:15 +0530

    Create day_13.py

---
+++ day_13.py
@@ -0,0 +1,8 @@
+my_watchlist = ["gold", "nifty"]
+print(f"Opening Bell Watchlist: {my_watchlist}")
+
+new_target = "silver"
+my_watchlist.append(new_target)
+
+print(f"Closing Bell Watchlist: {my_watchlist}")
+print(f"Total Targets: {len(my_watchlist)}")
