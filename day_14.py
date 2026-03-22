613d5bc94daa5cf3121239dd3c2dde6e068af24e
favie <arah12anshi@gmail.com>
2026-03-22 20:12:46 +0530

    Create day_14.py

---
+++ day_14.py
@@ -0,0 +1,30 @@
+watchlist = ["gold" , "nifty","bitcoin", "apple"]
+print(f"original watchlist: {watchlist}")
+watchlist.remove("nifty")
+removed ="nifty"
+print(f"removed:{removed}")
+
+target = ["plan", "planned" , "code" ,"planning"]
+print(f"original target : { target}")
+target.remove("code")
+remove = "code"
+print(f"remove : {remove}")
+
+market = ["forex", "crypto", "indian"]
+print(f"original goal :{market}")
+market.remove("indian")
+removed = "indian"
+print(f"remove : {removed}")
+
+
+trading_platform = ["mt5", "trading view", "angel"]
+print(f"best : {trading_platform}")
+trading_platform.remove("mt5")
+remove="mt5"
+print(f"removed : {remove}")
+
+
+
+
+
+
