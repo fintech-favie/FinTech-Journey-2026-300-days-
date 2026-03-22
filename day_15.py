69ffa7a6231b66f8dd267dc4cb56f8df069d4bdc
favie <arah12anshi@gmail.com>
2026-03-22 22:49:54 +0530

    Create day_15.py

---
+++ day_15.py
@@ -0,0 +1,22 @@
+my_goals = ["gold","nifty","bitcoin"]
+my_goals.sort()
+print(f"priority view  (A-Z) : {my_goals}")
+print(f"total assets tracked : {len(my_goals)}")
+
+my_goals.reverse()
+print(f"priority view (Z- A) : {my_goals}")
+print(f"total assets tracked : {len(my_goals)}")
+
+order = ["b" , "a" ,"c"]
+order.sort()
+print(f"priority view (A-Z): {order}")
+print(f"total assets tracked : {len(order)}")
+
+order.reverse()
+print(f"priority view (Z-A): {order}")
+print(f"total assets tracked : {len(order)}")
+
+
+
+
+
