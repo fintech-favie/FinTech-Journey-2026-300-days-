c972bccd5a06bc6f321f09252f3b0b85eefa8666
favie <arah12anshi@gmail.com>
2026-03-24 22:15:39 +0530

    Create day_17.py

---
+++ day_17.py
@@ -0,0 +1,11 @@
+price = int("1000")
+if price <= 5000 :
+	print("alert:market crash ! invest heavily")
+elif price <= 7000:
+	print("signal : discount  zone buy small")
+elif price >= 100000:
+	print("signal over valued sell now for profit")
+else :
+	print("neutral zone do nothing")
+	
+	
