a82903ca412f8ccc2b4e92d6253fa870094feea2
favie <arah12anshi@gmail.com>
2026-03-30 16:40:48 +0530

    Create day_23.py

---
+++ day_23.py
@@ -0,0 +1,8 @@
+tools = ["python", "account", "trading","fintech", "logic","learnt"]
+print("accessing the digital ledger")
+print(f"primary skill : {tools[0]}")
+print(f" secondary skill : {tools[2]}")
+print(f"tertiary skill : {tools[3]}")
+print(f"fourth skill : {tools[4]}")
+print(f"fifth skill: {tools[5]}")
+print(f"full skillset:{tools}")
