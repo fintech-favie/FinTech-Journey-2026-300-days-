aff1ab5373794af87111e16946187af565ff4ed8
favie <arah12anshi@gmail.com>
2026-03-30 16:49:45 +0530

    Update day_23.py

--- day_23.py
+++ day_23.py
@@ -6,3 +6,10 @@ print(f"tertiary skill : {tools[3]}")
 print(f"fourth skill : {tools[4]}")
 print(f"fifth skill: {tools[5]}")
 print(f"full skillset:{tools}")
+
+fruits = ["apple","banana","cherry"]
+print("accesing the digital ledger")
+print(f"primary fruit : {fruits[0]}")
+print(f"secondary fruit : {fruits[1]}")
+print(f"tertiary fruit : {fruits[2]}")
+print(f"all fruits:{fruits}")
