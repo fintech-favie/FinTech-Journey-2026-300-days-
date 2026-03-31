4ec580dd29b971352e2971d858fbf5c518f92ccf
favie <arah12anshi@gmail.com>
2026-03-31 18:37:16 +0530

    Create day_24.py

---
+++ day_24.py
@@ -0,0 +1,15 @@
+
+skills = ["python","accountancy"]
+print(f"current skill : {skills}")
+skills.append("fintech")
+skills.append("data logic")
+print(f"updated skills:{skills}")
+print(f"latest achievement : {skills[2]}")
+
+ledger =[ ]
+print("opening ledger")
+ledger.append(500)
+ledger.append(1200)
+ledger.append(200)
+print(f"today's transaction: {ledger}")
+print(f"total entries :{len(ledger)}")
