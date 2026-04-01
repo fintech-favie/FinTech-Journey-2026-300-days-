026b614104d9f9ac287286bfc7ecbe363334d69a
favie <arah12anshi@gmail.com>
2026-04-01 21:20:04 +0530

    Create day_25.py

---
+++ day_25.py
@@ -0,0 +1,10 @@
+targets = ["coding" ,"microsft", "45lpa"]
+print(f"original targets : {targets}")
+targets[0] = "45lpa fintech career"
+print("data optimised")
+print(f"updated target :{targets}")
+
+fruits = ["banana", "apple", "orange"]
+print(f"changed fruit :{fruits}")
+fruits[1] = "pomegranate"
+print(f"updated fruits : {fruits}")
