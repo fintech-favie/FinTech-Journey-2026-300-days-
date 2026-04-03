7528d07513f57ee6c85ba686892137bfc8c21950
favie <arah12anshi@gmail.com>
2026-04-03 19:13:55 +0530

    Create day_26.py

---
+++ day_26.py
@@ -0,0 +1,13 @@
+portfolio = ["gold","nifty","bitcoin","silver"]
+print(f"current portfolio :{portfolio}")
+position =portfolio.index("bitcoin")
+print("search completed")
+print(f"bitcoin is located at Index : {position}")
+print(f"verified asset at index {position}:{portfolio[position]}")
+
+
+fruits=["apple","orange", "banana"]
+print("real fruits :{fruits}")
+position = fruits.index("orange")
+print("search completed")
+print(f"apple is located at index:{position}")
