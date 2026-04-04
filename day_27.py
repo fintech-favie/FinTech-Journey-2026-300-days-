f68e9489a1e4a8f700e94e7ec120907f18fa0395
favie <arah12anshi@gmail.com>
2026-04-04 17:33:09 +0530

    Create day_27.py

---
+++ day_27.py
@@ -0,0 +1,13 @@
+focus_list = ["coding","trading","school","social"]
+print(f"morning list :{focus_list}")
+focus_list.remove("social")
+print("optimization completed")
+print(f"updated list : {focus_list}")
+print(f"total task remaining : {len(focus_list)}")
+
+portfolio = ["gold","silver","bitcoin","nifty"]
+print(f"my portfolio :{portfolio}")
+portfolio.remove("silver")
+print("optimized")
+print("updated : {portfolio}")
+print(f"total chart remaining : {len(portfolio)}")
