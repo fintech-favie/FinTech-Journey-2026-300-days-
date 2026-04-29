4030a45bb924384209d66b0ac4c4ab0e2dd981a8
favie <arah12anshi@gmail.com>
2026-04-29 16:34:32 +0530

    Create day_50.py

---
+++ day_50.py
@@ -0,0 +1,6 @@
+print("----day50: addition module----")
+val1 = float(input("enter first value(e.g.,stock price):"))
+val2 = float(input("enter second value(e.g.,profit):"))
+total = val1+val2
+print("-" * 25)
+print(f"grand Total: {total} ")
