8b14394eca1ea875157c337e7ed7be937b1623cf
favie <arah12anshi@gmail.com>
2026-04-30 16:06:02 +0530

    Delete day_51.py

--- day_51.py
+++
@@ -1,6 +0,0 @@
-print("----day 52: scaling and quantity ----")
-price = float(input("enter price per unit:"))
-quantity = float(input("enter quantity:"))
-total_value = price * quantity 
-print("-" * 30)
-print(f"subtotal: ${total_value}") 
