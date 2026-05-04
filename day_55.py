93c59da6b1d85fd9c656089a4963013967313025
favie <arah12anshi@gmail.com>
2026-05-04 17:47:13 +0530

    Create day_55.py

---
+++ day_55.py
@@ -0,0 +1,11 @@
+# Day 55: Revision & Reinforcement
+print("--- 🔄 DAY 55: REVISION ---")
+
+# The Goal: Perfecting the Safety Gate
+x = float(input("Enter first number: "))
+y = float(input("Enter second number: "))
+
+if y != 0:
+    print("Final Result:", x / y)
+else:
+    print("Alert: Division by zero is impossible!")
